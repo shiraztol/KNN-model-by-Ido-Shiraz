fe2e38bdd83faa47de31000434ee02b92e17826f
Ido Fuchs <idofuchs@Idos-MacBook-Pro.local>
2022-08-26 18:48:45 +0300

    creation of server file

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(_name_)
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    
+    # If a form is submitted
+    if request.method == "POST":
+        
+        # Unpickle classifier
+        clf = joblib.load("clf.pkl")
+        
+        # Get values through input bars
+        height = request.form.get("height")
+        weight = request.form.get("weight")
+        
+        # Put inputs to dataframe
+        X = pd.DataFrame([[height, weight]], columns = ["Height", "Weight"])
+        
+        # Get prediction
+        prediction = clf.predict(X)[0]
+        
+    else:
+        prediction = ""
+        
+    return render_template("website.html", output = prediction)
+
+if _name_ == "_main_":
+  app.run()
