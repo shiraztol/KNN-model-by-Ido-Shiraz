68d92ca2fdc6f0f5686bdf014f0e5d2c338348d2
Ido Fuchs <idofuchs@Idos-MacBook-Pro.local>
2022-08-27 23:34:17 +0300

    adding changes

--- server.py
+++ server.py
@@ -1,7 +1,36 @@
+# from flask import Flask, render_template, request
+# import pandas as pd
+# import joblib
+# import pickle
+
+# app = Flask(__name__)
+
+# @app.route('/', methods=['GET', 'POST'])
+# def main():
+#     # If a form is submitted
+#     if request.method == "POST":
+#         # Unpickle classifier
+#         model = joblib.load("clf.pkl")
+#         # Get values through input bars
+#         image = request.form.get("client image")
+#         # Put inputs to dataframe
+#         X = pd.DataFrame([[image]], columns = ["image"], dtype = "str")
+#         # Get prediction
+#         prediction = model.predict(X)[0]
+#     else:
+#         prediction = " "   
+#     return render_template("website.html", output = prediction)
+
+# if __name__ == "_main_":
+#   app.run()
+
+
+
+
 from flask import Flask, render_template, request
-import pandas as pd
-import joblib
 import pickle
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -10,16 +39,18 @@ def main():
     # If a form is submitted
     if request.method == "POST":
         # Unpickle classifier
-        model = joblib.load("clf.pkl")
+        model = pickle.load("clf.pkl")
         # Get values through input bars
         image = request.form.get("client image")
         # Put inputs to dataframe
-        X = pd.DataFrame([[image]], columns = ["image"], dtype = "str")
+        X = pd.DataFrame([image])
         # Get prediction
         prediction = model.predict(X)[0]
     else:
         prediction = " "   
     return render_template("website.html", output = prediction)
 
-if __name__ == "_main_":
+if __name__ == "__main__":
   app.run()
+
+
