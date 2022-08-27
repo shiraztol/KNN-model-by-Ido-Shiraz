8c500bf33a888eefe851183463f7d1a24654aad2
Ido Fuchs <idofuchs@Idos-MacBook-Pro.local>
2022-08-27 17:41:33 +0300

    new

--- server.py
+++ server.py
@@ -1,28 +1,24 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import joblib
+import pickle
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    
     # If a form is submitted
     if request.method == "POST":
-        
         # Unpickle classifier
-        clf = joblib.load("clf.pkl")
-        
+        model = joblib.load("clf.pkl")
         # Get values through input bars
         image = request.form.get("client image")
-        
         # Put inputs to dataframe
-        X = pd.DataFrame([[height, weight]], columns = ["Height", "Weight"])
-        
+        X = pd.DataFrame([[image]], columns = ["image"], dtype = "str")
         # Get prediction
-        prediction = clf.predict(X)[0]
-        
+        prediction = model.predict(X)[0]
     else:
-        prediction = ""
-        
+        prediction = " "   
     return render_template("website.html", output = prediction)
 
 if __name__ == "_main_":
