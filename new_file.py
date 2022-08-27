fadcf75bde9064b26192f4cc6b5d3d66ac2cf883
שירז טולדנו <shirazmacbook@h-MacBook-Pro-sl-syrz.local>
2022-08-27 20:09:19 +0300

    changes by shizi

--- new_file.py
+++ new_file.py
@@ -1,6 +1,39 @@
+
+
 import pickle
 import pymongo
 import pandas as pd
+
+def most_common(lst):
+    '''Returns the most common element in a list'''
+    return max(set(lst), key=lst.count)
+
+
+def euclidean(point, data):
+    '''Euclidean distance between a point  & data'''
+    return np.sqrt(np.sum((point - data)**2, axis=1))
+
+
+
+
+class KNeighborsClassifier_IdoShiraz():
+    def __init__(self, k=5, dist_metric=euclidean):
+        self.k = k
+        self.dist_metric = dist_metric
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+    def predict(self, X_test):
+        neighbors = []
+        for x in X_test:
+            distances = self.dist_metric(x, self.X_train)
+            y_sorted = [y for _, y in sorted(zip(distances, self.y_train))]
+            neighbors.append(y_sorted[:self.k])
+        return list(map(most_common, neighbors))
+    def evaluate(self, X_test, y_test):
+        y_pred = self.predict(X_test)
+        accuracy = sum(y_pred == y_test) / len(y_test)
+        return accuracy
 
 
 def save_model_to_mongo(model):
